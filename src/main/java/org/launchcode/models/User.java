edacdb4a136971c80b3cb7347312814090a5a146
bluzL <menon_kavita@yahoo.com>
2024-10-17 14:06:31 -0500

    Chapter 15: Studio Spa User - Part 2

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @Size(min=3, max=15, message="Username must be 5-15 characters long.")
     private String username;
+
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @Size(min=6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotNull(message = "Passwords do not match or are empty.")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword=verifyPassword;
     }
 
     public String getUsername() {
@@ -38,6 +51,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    public void checkPassword(){
+          if(password != null && verifyPassword != null && !password.equals(verifyPassword)){
+              setVerifyPassword(null);
+          }
     }
 }
 
